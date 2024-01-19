3c46c3bb0b28c3670cf62b6485b7688c2c6c849f
fu19841 <fu19841@bristol.ac.uk>
2024-01-19 13:48:37 +0000

    Small efficiency improvements, still needs parallelising etc

--- testing/est_embed/generate_vec_estimates.py
+++ testing/est_embed/generate_vec_estimates.py
@@ -47,15 +47,6 @@ node_list = pd.read_csv(
 node_name_to_index = {row[1]: row[0] for _, row in node_list.iterrows()}
 node_index_to_name = {row[0]: row[1] for _, row in node_list.iterrows()}
 
-# Get relation info
-rel_embeds = model.state_dict()['_relation_embedder._embeddings.weight']
-rel_list = pd.read_csv(
-    checkpoint['config'].get('dataset.name') + '/relation_ids.del',
-    sep='\t', header=None
-)
-rel_name_to_index = {row[1]: row[0] for _, row in rel_list.iterrows()}
-rel_index_to_name = {row[0]: row[1] for _, row in rel_list.iterrows()}
-
 # Get list of drugs
 drugs = [key for key in node_name_to_index.keys() if key.startswith('CID')]
 
@@ -84,48 +75,57 @@ for test_drug in drugs:
         neighbours_index = [node_name_to_index[node] for node in neighbour_nodes]
         neighbours_embeds = node_embeds[neighbours_index]
 
+    # Store actual embeddings in the df too
+    actual_embed = node_embeds[node_name_to_index[test_drug]]
+    result = [test_drug, 'actual embedding', np.nan] + actual_embed.numpy().tolist()
+    results.loc[len(results)] = result
+
     # TODO: selecting 'stranger' stuff below should only be run if either..
     # ..'generalised inverse' or 'least squares' has not been run for this drug
 
-    # Randomly select equal number of non-neighbour drugs
-    all_strangers = set(drugs) - neighbour_nodes
-    stranger_nodes = np.random.choice(list(all_strangers), n_neighbours, replace=False)
-    strangers_index = [node_name_to_index[node] for node in stranger_nodes]
-    strangers_embeds = node_embeds[strangers_index]
+    existing_check = results.query(f'drug == "{test_drug}"')
 
-    # Combine all selected embeddings
-    test_mat = torch.cat((neighbours_embeds, strangers_embeds))
+    # Generate A and b if solving Ax=b to get x
+    stranger_check1 = 'Generalised inverse' not in existing_check['vector_method']
+    stranger_check2 = 'Least squares' not in existing_check['vector_method']
+    if stranger_check1 or stranger_check2:
+        # Randomly select equal number of non-neighbour drugs
+        all_strangers = set(drugs) - neighbour_nodes
+        stranger_nodes = np.random.choice(list(all_strangers), n_neighbours, replace=False)
+        strangers_index = [node_name_to_index[node] for node in stranger_nodes]
+        strangers_embeds = node_embeds[strangers_index]
 
-    # Create adjacency vector to approximate with factorisation
-    adj_vec = np.array([1 for _ in neighbour_nodes] + [0 for _ in stranger_nodes])
+        # Combine all selected embeddings
+        test_mat = torch.cat((neighbours_embeds, strangers_embeds))
+
+        # Create adjacency vector to approximate with factorisation
+        adj_vec = np.array([1 for _ in neighbour_nodes] + [0 for _ in stranger_nodes])
 
     ######################################
     # Solving Ax=b for x using generalised inverse of rectangular embeddings
     # Matrix test_mat is rectangular so have to take the generalised inverse to solve for my_vec..
     # .. where test_mat x my_vec = adj_vec
     method = 'Generalised inverse'
-    already_run = len(results.query(f'drug == "{test_drug}" & vector_method == "{method}"'))
+    already_run = method in existing_check['vector_method'].values
     if not already_run:
         rect_inverse = np.linalg.pinv(test_mat)
         my_vec = np.dot(rect_inverse, adj_vec)
         result = [test_drug, method, n_neighbours] + my_vec.tolist()
         results.loc[len(results)] = result
-        results.to_csv(outfile_name, index=False)
 
     #######################################
     # Solving Ax=b for x (as above) but using least squares instead
     method = 'Least squares'
-    already_run = len(results.query(f'drug == "{test_drug}" & vector_method == "{method}"'))
+    already_run = method in existing_check['vector_method'].values
     if not already_run:
         my_vec = np.linalg.lstsq(test_mat, adj_vec)[0]
         result = [test_drug, method, n_neighbours] + my_vec.tolist()
         results.loc[len(results)] = result
-        results.to_csv(outfile_name, index=False)
 
     #######################################
     # Take principal eigenvec of neighbour embeddings
     method = '1st eigenvec'
-    already_run = len(results.query(f'drug == "{test_drug}" & vector_method == "{method}"'))
+    already_run = method in existing_check['vector_method'].values
     if not already_run:
         if n_neighbours >= 2:
             my_vec = PCA().fit(neighbours_embeds).components_[1]
@@ -133,14 +133,15 @@ for test_drug in drugs:
         else:
             result = [test_drug, method, n_neighbours] + [np.nan for _ in range(len(my_vec))]
         results.loc[len(results)] = result
-        results.to_csv(outfile_name, index=False)
 
     #######################################
     # Take component-wise mean of neighbour embeddings
     method = 'mean columns'
-    already_run = len(results.query(f'drug == "{test_drug}" & vector_method == "{method}"'))
+    already_run = method in existing_check['vector_method'].values
     if not already_run:
         my_vec = mean_column_value(neighbours_embeds)
         result = [test_drug, method, n_neighbours] + my_vec
         results.loc[len(results)] = result
-        results.to_csv(outfile_name, index=False)
+
+    # Save df after each drug
+    results.to_csv(outfile_name, index=False)
